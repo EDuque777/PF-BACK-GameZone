d6c1344d1325a3c1908aab4a85fbdcb3faa8997e
EDuqueC7 <esteban.duque911@gmail.com>
2023-06-16 09:46:29 -0500

    nuevos cambios

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,6 @@
 const { Router } = require('express');
 const  saveGames = require("../controllers/saveGames")
 const {nameGames} = require("../controllers/nameGames")
-const {categoryGames} = require("../controllers/categoryGames")
-const {genresGames} = require("../controllers/genresGames")
-const {platformGames} = require("../controllers/platformGames")
-const {languageGames} = require("../controllers/languageGames")
-const {developersGames} = require("../controllers/developersGames")
-const {editorsGames} = require("../controllers/editorsGames")
 const {allGames} = require("../controllers/allGames")
 const {searchId} = require("../controllers/searchId")
 const comingSoon = require('../controllers/comingSoon')
@@ -36,30 +30,6 @@ router.get("/allGames", (req, res) => {
 
 router.get("/nameGames", (req, res) => {
     nameGames(req, res);
-})
-
-router.get("/categoryGames", (req, res) => {
-    categoryGames(req, res);
-})
-
-router.get("/genresGames", (req, res) => {
-    genresGames(req, res);
-})
-
-router.get("/platformGames", (req, res) => {
-    platformGames(req, res);
-})
-
-router.get("/languageGames", (req, res) => {
-    languageGames(req, res);
-})
-
-router.get("/developersGames", (req, res) => {
-    developersGames(req, res);
-})
-
-router.get("/editorsGames", (req, res) => {
-    editorsGames(req, res);
 })
 
 router.get("/search/:id", (req, res) => {
