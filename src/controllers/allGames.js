f9a78d4042456c42a8aaf5c3171c83aad515562b
EDuqueC7 <esteban.duque911@gmail.com>
2023-06-16 12:15:17 -0500

    Correccion del id en entero

--- src/controllers/allGames.js
+++ src/controllers/allGames.js
@@ -1,10 +1,19 @@
 const {Games, Developers, Publishers, Languages, Platforms, Genres, Categories} = require("../db")
+const axios = require('axios');
+require('dotenv').config();
+const { URL } = process.env;
+const gameUrl = 'https://store.steampowered.com/api/appdetails?appids=';
+
 
 const allGames = async(req, res) => {
 
     try {
 
+        const { data: appList } = await axios.get(URL);
+        const idGames = appList.applist.apps.filter(app => app.name.length > 0);
+
         const dbGames = await Games.findAll({
+            attributes: { exclude: ['id'] },
             include: [
               { model: Developers, attributes: ['developer'], through: { attributes: [] } },
               { model: Publishers, attributes: ['publisher'], through: { attributes: [] } },
@@ -15,7 +24,21 @@ const allGames = async(req, res) => {
             ]
         });
 
-        return res.status(200).json(dbGames);
+        const gamesWithId = dbGames.map(dbGame => {
+            const matchingGame = idGames.find(app => app.name === dbGame.name);
+      
+            if (matchingGame) {
+              return {
+                ...dbGame.toJSON(),
+                id: matchingGame.appid
+              };
+            }
+      
+            return dbGame.toJSON();
+          });
+
+          return res.status(200).json(gamesWithId);
+
 
     } catch (error) {
 
