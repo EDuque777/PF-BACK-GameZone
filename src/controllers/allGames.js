d89ff13239be25947c6827470d1f0c8a4930111e
EDuqueC7 <esteban.duque911@gmail.com>
2023-06-16 17:43:43 -0500

    Correccion de valor Id y Precio USD

--- src/controllers/allGames.js
+++ src/controllers/allGames.js
@@ -1,53 +1,110 @@
-const {Games, Developers, Publishers, Languages, Platforms, Genres, Categories} = require("../db")
+// const {Games, Developers, Publishers, Languages, Platforms, Genres, Categories} = require("../db")
+// const axios = require('axios');
+// require('dotenv').config();
+// const { URL } = process.env;
+
+// const allGames = async(req, res) => {
+
+//     try {
+
+//         const { data: appList } = await axios.get(URL);
+//         const idGames = appList.applist.apps.filter(app => app.name.length > 0);
+
+//         const dbGames = await Games.findAll({
+//             attributes: { exclude: ['id'] },
+//             include: [
+//               { model: Developers, attributes: ['developer'], through: { attributes: [] } },
+//               { model: Publishers, attributes: ['publisher'], through: { attributes: [] } },
+//               { model: Languages, attributes: ['language'], through: { attributes: [] } },
+//               { model: Platforms, attributes: ['platform'], through: { attributes: [] } },
+//               { model: Genres, attributes: ['genre'], through: { attributes: [] } },
+//               { model: Categories, attributes: ['category'], through: { attributes: [] } },
+//             ]
+//         });
+
+//         const gamesWithId = dbGames.map(dbGame => {
+//         const matchingGame = idGames.find(app => app.name === dbGame.name);
+      
+//             if (matchingGame) {
+//               return {
+//                 ...dbGame.toJSON(),
+//                 id: matchingGame.appid
+//               };
+//             }
+      
+//             return dbGame.toJSON();
+//           });
+
+//           return res.status(200).json(gamesWithId);
+
+
+//     } catch (error) {
+
+//         res.status(404).send(error.message);
+    
+//     }
+
+// }
+
+// module.exports = {
+//     allGames
+// }
+
+
+
+const { Games, Developers, Publishers, Languages, Platforms, Genres, Categories } = require("../db");
 const axios = require('axios');
 require('dotenv').config();
 const { URL } = process.env;
-const gameUrl = 'https://store.steampowered.com/api/appdetails?appids=';
 
+const allGames = async (req, res) => {
+  try {
+    const { data: appList } = await axios.get(URL);
+    const idGames = appList.applist.apps.filter(app => app.name.length > 0);
 
-const allGames = async(req, res) => {
+    const dbGames = await Games.findAll({
+      attributes: { exclude: ['id'] },
+      include: [
+        { model: Developers, attributes: ['developer'], through: { attributes: [] } },
+        { model: Publishers, attributes: ['publisher'], through: { attributes: [] } },
+        { model: Languages, attributes: ['language'], through: { attributes: [] } },
+        { model: Platforms, attributes: ['platform'], through: { attributes: [] } },
+        { model: Genres, attributes: ['genre'], through: { attributes: [] } },
+        { model: Categories, attributes: ['category'], through: { attributes: [] } },
+      ]
+    });
 
-    try {
+    const gamesWithId = dbGames.map(dbGame => {
+    const matchingGame = idGames.find(app => app.name === dbGame.name);
 
-        const { data: appList } = await axios.get(URL);
-        const idGames = appList.applist.apps.filter(app => app.name.length > 0);
+      if (matchingGame) {
+        return {
+          ...dbGame.toJSON(),
+          id: matchingGame.appid
+        };
+      }
 
-        const dbGames = await Games.findAll({
-            attributes: { exclude: ['id'] },
-            include: [
-              { model: Developers, attributes: ['developer'], through: { attributes: [] } },
-              { model: Publishers, attributes: ['publisher'], through: { attributes: [] } },
-              { model: Languages, attributes: ['language'], through: { attributes: [] } },
-              { model: Platforms, attributes: ['platform'], through: { attributes: [] } },
-              { model: Genres, attributes: ['genre'], through: { attributes: [] } },
-              { model: Categories, attributes: ['category'], through: { attributes: [] } },
-            ]
-        });
+      return dbGame.toJSON();
+    });
 
-        const gamesWithId = dbGames.map(dbGame => {
-            const matchingGame = idGames.find(app => app.name === dbGame.name);
-      
-            if (matchingGame) {
-              return {
-                ...dbGame.toJSON(),
-                id: matchingGame.appid
-              };
-            }
-      
-            return dbGame.toJSON();
-          });
-
-          return res.status(200).json(gamesWithId);
-
-
-    } catch (error) {
-
-        res.status(404).send(error.message);
-    
+    const gamesWithModifiedPrice = gamesWithId.map(game => {
+      if (game.price_overview === "Free") {
+        game.price_overview = 0;
+      } else {
+        const price = game.price_overview.replace(/[^0-9]/g, '');
+        game.price_overview = parseFloat(price) * 0.00024;
     }
+      return game;
+    });
 
-}
+    return res.status(200).json(gamesWithModifiedPrice);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+};
 
 module.exports = {
-    allGames
-}
+  allGames
+};
+
+
--- src/controllers/nameGames.js
+++ src/controllers/nameGames.js
@@ -1,11 +1,17 @@
 const {Games, Developers, Publishers, Languages, Platforms, Genres, Categories} = require("../db")
 const Sequelize = require('sequelize');
+const axios = require('axios');
+require('dotenv').config();
+const { URL } = process.env;
 
 const nameGames = async(req, res) => {
 
     try {
 
         const {name} = req.query;
+
+        const { data: appList } = await axios.get(URL);
+        const idGames = appList.applist.apps.filter(app => app.name.length > 0);
 
         const dbGames = await Games.findAll({
             where: {name: {[Sequelize.Op.iLike]: `%${name}%`}},
@@ -19,7 +25,32 @@ const nameGames = async(req, res) => {
             ]
         })
 
-        return res.status(200).json(dbGames);
+        const gamesWithId = dbGames.map(dbGame => {
+            const matchingGame = idGames.find(app => app.name === dbGame.name);
+        
+              if (matchingGame) {
+                return {
+                  ...dbGame.toJSON(),
+                  id: matchingGame.appid
+                };
+              }
+        
+              return dbGame.toJSON();
+            });
+        
+            const gamesWithModifiedPrice = gamesWithId.map(game => {
+              if (game.price_overview === "Free") {
+                game.price_overview = 0;
+              } else {
+                const price = game.price_overview.replace(/[^0-9]/g, '');
+                game.price_overview = parseFloat(price) * 0.00024;
+            }
+              return game;
+            });
+        
+            return res.status(200).json(gamesWithModifiedPrice);
+
+        //return res.status(200).json(dbGames);
 
     } catch (error) {
 
