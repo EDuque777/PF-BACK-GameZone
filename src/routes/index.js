844d8ec818ebbb3ce0974e595b5107a1d9c9596a
EDuqueC7 <esteban.duque911@gmail.com>
2023-06-16 10:28:34 -0500

    Cambios de cristian agregados

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,6 +17,8 @@ const { validateToken } = require("../middlewares/validateToken.js")
 //const {postRecipes} = require("../controllers/postRecipes")
 //const {getDiets} = require("../controllers/getDiets")
 //const {login} = require("../controllers/login")
+const { getAllGames, getGame, createGames, deleteGame, updateGame, banGame } = require('../controllers/games.controllers');
+const { getAllUsers, getUser, createUser, deleteUser, updateUser, banUser } = require('../controllers/users.controllers');
 
 const router = Router();
 
@@ -83,6 +85,38 @@ router.post("/cerrarSesion", cerrarSesion)
 
 // Ruta del perfil del Usuario (esto es solo un ejemplo, se encarga Cristian)
 // esto sera como una ruta protegida
-router.get("/profile", validateToken, profileUser)
+router.get("/profile", validateToken, profileUser);
+
+
+
+// Ruta para tarer todos los Games (borrado lógico)
+router.get('/games', getAllGames);
+// Ruta para tarer un Game (borrado lógico)
+router.get('/games/:id', getGame);
+// Ruta para crear un Game (borrado lógico)
+router.post('/games', createGames);
+// Ruta para eliminar un Games por id (borrado lógico)
+router.delete('/games/:id', deleteGame);
+// Ruta para actualizar datos un Game (borrado lógico)
+router.put('/games/:id', updateGame);
+// Ruta para banear un Game (borrado lógico)
+router.put('/games/:gamesId/ban', banGame);
+
+
+
+// Ruta para tarer todos los usuario (borrado lógico)
+router.get('/users', getAllUsers);
+// Ruta para tarer un usuario (borrado lógico)
+router.get('/users/:id', getUser);
+// Ruta para crear un usuario (borrado lógico)
+router.post('/users', createUser);
+// Ruta para eliminar un usuario por id (borrado lógico)
+router.delete('/users/:id', deleteUser);
+// Ruta para actualizar datos un usuario (borrado lógico)
+router.put('/users/:id', updateUser);
+// Ruta para banear un usuario (borrado lógico)
+router.put('/users/:userId/ban', banUser);
+
+
 
 module.exports = router;
