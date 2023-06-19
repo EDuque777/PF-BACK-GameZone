13d0d5d4417444198840219538ffad3e5bf2f5b8
fernandotejada1997 <fernandotejada1997@gmail.com>
2023-06-19 14:40:48 -0300

    ramafernando

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+
 const port = process.env.PORT || 3001;
 
 conn.sync({ force: false, alter : true }).then(() => {
