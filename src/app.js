fa1a777684e69531e36c29820e4c710c8128321e
DantePc <djkmecdgm65@gmail.com>
2023-06-20 01:36:57 -0500

    descargue cors y hice una modificaciones en createaccount

--- src/app.js
+++ src/app.js
@@ -4,10 +4,14 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
 const session = require("express-session")
+const cors = require("cors")
 
 require('./db.js');
 
 const server = express();
+server.use(cors())
+server.use(express.json())
+server.use(morgan('dev'));
 
 server.name = 'API';
 
@@ -21,7 +25,6 @@ server.use(session(
     saveUninitialized:false, 
   }
 ))
-server.use(morgan('dev'));
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); 
   res.header('Access-Control-Allow-Credentials', 'true');
--- src/controllers/createAccount.js
+++ src/controllers/createAccount.js
@@ -1,16 +1,18 @@
 const { Users } = require("../db")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
-
 const { createAccessToken } = require("../middlewares/jwt.js")
+const express = require('express');
+const router = express.Router();
 
 require("dotenv").config()
 
-const { JWT_SECRET } = process.env
+const { JWT_SECRET, URL_INICIO } = process.env
 
 const createAccount = async (req, res) => {
     try {
-        const { email, password, name, user_name, country } = req.body
+
+        const { email, password, name, user_name, country} = req.body
 
         if (!email || password.length < 8 || !password || !name || !country || !user_name ) {
             
@@ -28,12 +30,12 @@ const createAccount = async (req, res) => {
 
                 res.status(400).json({message : "el usuario ya existe!!!"})
 
-            } else {
+            }else{
 
                 const salt = await bcrypt.genSalt(12)
 
                 const cripto = await bcrypt.hash(password, salt)
-
+    
                 const createUser = await Users.create({
                     name,
                     email,
@@ -41,27 +43,17 @@ const createAccount = async (req, res) => {
                     user_name,
                     country
                 })
-
+    
                 // esto se convierte en un middlewares
                 // para validar el token tenemos que crear el login
-                const token = await createAccessToken({id : createUser.id})
+                const token = await createAccessToken({id : createUser.id}) // esto podria se opcional
                 //const token = jwt.sign({id : createUser.id}, JWT_SECRET, {expiresIn : "1d"})
-
-                console.log(token, " esto es el token")
-
+                //console.log(token, " esto es el token")
+    
                 res.cookie("token", token)
-
-                res.status(201).json(// envio los datos del usuario recien registrado(opcional)
-                    {
-                        message : "Usuario Creado !!",
-                        name : name,
-                        email : email,
-                        user_name : user_name,
-                        country : country
-                    }
-                )
-
-                console.log(createUser)
+                res.status(200).json({
+                    message : "Usuario Creado"
+                })
             }
         }
 
@@ -69,6 +61,18 @@ const createAccount = async (req, res) => {
         res.status(500).json({message : error.message})
     }
 }
+
+//const redirectToHome = (req, res) => {
+//    try {
+//
+//        console.log(URL_INICIO)
+//
+//        return res.redirect(URL_INICIO)
+//        
+//    } catch (error) {
+//        res.status(500).json({message : error.message})
+//    }
+//}
 
 module.exports = {
     createAccount
