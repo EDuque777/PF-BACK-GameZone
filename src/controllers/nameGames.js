49ea1f8f78489a176385954efb471019aba09856
JeroBl <jeroblanco44@gmail.com>
2023-06-21 11:38:27 -0300

    abc

--- src/controllers/nameGames.js
+++ src/controllers/nameGames.js
@@ -1,34 +1,37 @@
-const {Games, Developers, Publishers, Languages, Platforms, Genres, Categories} = require("../db")
-const Sequelize = require('sequelize');
+const axios = require('axios');
+require('dotenv').config();
+const { URL } = process.env;
 
-const nameGames = async(req, res) => {
+const nameGames = async (req, res) => {
+const { name } = req.query;
+const justname = name;
+console.log(name)
+console.log(justname)
 
-    try {
+try {
+    const { data } = await axios.get(URL);
+    const alldata = data.applist.apps;
 
-        const {name} = req.query;
+    // Filtrar las coincidencias en base al nombre
+    const filteredData = alldata.filter(game => game.name.toLowerCase().includes(justname.toLowerCase()));
 
-        const dbGames = await Games.findAll({
-            where: {name: {[Sequelize.Op.iLike]: `%${name}%`}},
-            include: [
-                { model: Developers, attributes: ['developer'], through: { attributes: [] } },
-                { model: Publishers, attributes: ['publisher'], through: { attributes: [] } },
-                { model: Languages, attributes: ['language'], through: { attributes: [] } },
-                { model: Platforms, attributes: ['platform'], through: { attributes: [] } },
-                { model: Genres, attributes: ['genre'], through: { attributes: [] } },
-                { model: Categories, attributes: ['category'], through: { attributes: [] } },
-            ]
+    // Obtener los datos adicionales de cada juego
+    const gamesWithData = await Promise.all(
+        filteredData.slice(0, 14).map(async game => {
+        const { appid } = game;
+        const response = await axios.get(`https://store.steampowered.com/api/appdetails?appids=${appid}`);
+        const gameData = response.data[appid].data;
+        return { ...game, ...gameData };
         })
+    );
 
-        return res.status(200).json(dbGames);
+    return res.status(200).json(gamesWithData);
 
     } catch (error) {
-
-        res.status(404).send(error.message);
-    
+    res.status(404).send(error.message);
     }
-
-}
+};
 
 module.exports = {
     nameGames
-}
+};
