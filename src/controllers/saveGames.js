f5b744a8c898211c56b28b3a3559ce2e17c5cd02
Murdock021 <diogomachadocmb@gmail.com>
2023-06-23 18:34:39 -0300

    Perfecto

--- src/controllers/saveGames.js
+++ src/controllers/saveGames.js
@@ -9,8 +9,8 @@ const saveGames = async (req, res) => {
     const { data: appList } = await axios.get(URL);
     const idGames = appList.applist.apps.filter(app => app.name.length > 0);
 
-    for (let i = 0; i <= 1000; i++) {
-      await new Promise(resolve => setTimeout(resolve, 2000));
+    for (let i = 0; i <= 20; i++) {
+      /* await new Promise(resolve => setTimeout(resolve, 2000)); */
       const { data } = await axios.get(`${gameUrl}${idGames[i].appid}`);
       const info = data[idGames[i].appid.toString()].data;
       if (info) {
