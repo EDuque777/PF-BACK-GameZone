777c77d1718b9c59c329c6a28a9ec0bf92a41456
fernandotejada1997 <fernandotejada1997@gmail.com>
2023-06-29 20:54:32 -0300

    PRE_RENDER

--- src/db.js
+++ src/db.js
@@ -28,7 +28,7 @@ const {
 // const sequelize = new Sequelize(DB_RAILWAY, {
 //  logging: false, // set to console.log to see the raw SQL queries
 //  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-// });
+// })
 
 
 // const sequelize = new Sequelize(DB_RENDER , {
