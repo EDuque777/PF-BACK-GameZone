1d59b9afe2c0abde09b46e08efab3ab4cbc2a61c
fernandotejada1997 <fernandotejada1997@gmail.com>
2023-06-30 18:16:04 -0300

    cambios back

--- src/controllers/nameGames.js
+++ src/controllers/nameGames.js
@@ -1,86 +1,128 @@
-const {Games, Developers, Languages, Platforms, Genres, Categories, Images, Videos} = require("../db")
+const { Games, Developers, Languages, Platforms, Genres, Categories, Images, Videos } = require("../db");
 const Sequelize = require('sequelize');
-const axios = require('axios');
-require('dotenv').config();
-const { URL } = process.env;
+
 const nameGames = async (req, res) => {
-const { name } = req.query;
+  const { name } = req.query;
 
-try {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
 
-  const page = parseInt(req.query.page) || 1;
-  const limit = parseInt(req.query.limit) || 5;
+    const dbGames = await Games.findAll({
+      where: { name: { [Sequelize.Op.iLike]: `%${name}%` } },
+      attributes: { exclude: ['id'] },
+      include: [
+        { model: Developers, attributes: ['developer'], through: { attributes: [] } },
+        { model: Languages, attributes: ['language'], through: { attributes: [] } },
+        { model: Platforms, attributes: ['platform'], through: { attributes: [] } },
+        { model: Genres, attributes: ['genre'], through: { attributes: [] } },
+        { model: Categories, attributes: ['category'], through: { attributes: [] } },
+        { model: Images, attributes: ['image'], through: { attributes: [] } },
+        { model: Videos, attributes: ['video'], through: { attributes: [] } },
+      ],
+      offset: (page - 1) * limit,
+      limit: limit
+    });
 
-        // const { data: appList } = await axios.get(URL);
-        // const idGames = appList.applist.apps.filter(app => app.name.length > 0);
-        // const transformPrice = "https://v6.exchangerate-api.com/v6/27580267bfc72f9a7ad8b625/latest/USD";
-        // const { data: priceData } = await axios.get(transformPrice);
-        // const conversionRates = priceData.conversion_rates;
-
-        const dbGames = await Games.findAll({
-            where: {name: {[Sequelize.Op.iLike]: `%${name}%`}},
-            attributes: { exclude: ['id'] },
-            include: [
-                { model: Developers, attributes: ['developer'], through: { attributes: [] } },
-                { model: Languages, attributes: ['language'], through: { attributes: [] } },
-                { model: Platforms, attributes: ['platform'], through: { attributes: [] } },
-                { model: Genres, attributes: ['genre'], through: { attributes: [] } },
-                { model: Categories, attributes: ['category'], through: { attributes: [] } },
-                { model: Images, attributes: ['image'], through: { attributes: [] } },
-                { model: Videos, attributes: ['video'], through: { attributes: [] } },
-            ],
-            offset: (page - 1) * limit,
-            limit: limit
-        }
-    );
-
-        const gamesWithId = dbGames.map(dbGame => {
-        const matchingGame = idGames.find(app => app.name === dbGame.name);
-        
-            if (matchingGame) {
-              return {
-                id: matchingGame.appid,
-                ...dbGame.toJSON(),
-              };
-            }
-            return null;
-            //return dbGame.toJSON();
-          });
-      
-          const filteredGames = gamesWithId.filter(game => game !== null);
-        
-            const gamesWithModifiedPrice = filteredGames.map(game => {
-              if (game.price_overview === "Free") {
-                game.price_overview = 0;
-              } 
-              else {
-                const currency01 = game.currency;
-                if(currency01 !== "USD"){
-                const currency01 = game.currency;
-                if(currency01 === "COP"){
-                  const currencyPrice = game.price_overview.replace(/[^0-9]/g, '');
-                  const convertedPrice = (currencyPrice / conversionRates[currency01]).toFixed(2);
-                  game.price_overview = convertedPrice;
-                }
-                else{
-                  const currencyPrice = game.price_overview.replace(/(\d)(?=(\d{3})+(?!\d))/g, '1.').replace(/.\d+$/, '').replace(/[^0-9]/g, '');
-                  const convertedPrice = (currencyPrice / conversionRates[currency01]).toFixed(2);
-                  game.price_overview = convertedPrice;
-                }
-              }
-            }
-              return game;
-            });
-        
-            return res.status(200).json(gamesWithModifiedPrice);
-
-    } catch (error) {
+    return res.status(200).json(dbGames);
+  } catch (error) {
     res.status(404).send(error.message);
-    }
+  }
 };
+
 module.exports = {
-    nameGames
-}
+  nameGames
+};
+
+
+
+
+
+
+
+// const {Games, Developers, Languages, Platforms, Genres, Categories, Images, Videos} = require("../db")
+// const Sequelize = require('sequelize');
+// const axios = require('axios');
+// require('dotenv').config();
+// const { URL } = process.env;
+// const nameGames = async (req, res) => {
+// const { name } = req.query;
+
+// try {
+
+//   const page = parseInt(req.query.page) || 1;
+//   const limit = parseInt(req.query.limit) || 5;
+
+//         const { data: appList } = await axios.get(URL);
+//         const idGames = appList.applist.apps.filter(app => app.name.length > 0);
+//         const transformPrice = "https://v6.exchangerate-api.com/v6/27580267bfc72f9a7ad8b625/latest/USD";
+//         const { data: priceData } = await axios.get(transformPrice);
+//         const conversionRates = priceData.conversion_rates;
+
+//         const dbGames = await Games.findAll({
+//             where: {name: {[Sequelize.Op.iLike]: `%${name}%`}},
+//             attributes: { exclude: ['id'] },
+//             include: [
+//                 { model: Developers, attributes: ['developer'], through: { attributes: [] } },
+//                 { model: Languages, attributes: ['language'], through: { attributes: [] } },
+//                 { model: Platforms, attributes: ['platform'], through: { attributes: [] } },
+//                 { model: Genres, attributes: ['genre'], through: { attributes: [] } },
+//                 { model: Categories, attributes: ['category'], through: { attributes: [] } },
+//                 { model: Images, attributes: ['image'], through: { attributes: [] } },
+//                 { model: Videos, attributes: ['video'], through: { attributes: [] } },
+//             ],
+//             offset: (page - 1) * limit,
+//             limit: limit
+//         }
+//     );
+
+//         const gamesWithId = dbGames.map(dbGame => {
+//         const matchingGame = idGames.find(app => app.name === dbGame.name);
+        
+//             if (matchingGame) {
+//               return {
+//                 id: matchingGame.appid,
+//                 ...dbGame.toJSON(),
+//               };
+//             }
+//             return null;
+//             //return dbGame.toJSON();
+//           });
+      
+//           const filteredGames = gamesWithId.filter(game => game !== null);
+        
+//             const gamesWithModifiedPrice = filteredGames.map(game => {
+//               if (game.price_overview === "Free") {
+//                 game.price_overview = 0;
+//               } 
+//               else {
+//                 const currency01 = game.currency;
+//                 if(currency01 !== "USD"){
+//                 const currency01 = game.currency;
+//                 if(currency01 === "COP"){
+//                   const currencyPrice = game.price_overview.replace(/[^0-9]/g, '');
+//                   const convertedPrice = (currencyPrice / conversionRates[currency01]).toFixed(2);
+//                   game.price_overview = convertedPrice;
+//                 }
+//                 else{
+//                   const currencyPrice = game.price_overview.replace(/(\d)(?=(\d{3})+(?!\d))/g, '1.').replace(/.\d+$/, '').replace(/[^0-9]/g, '');
+//                   const convertedPrice = (currencyPrice / conversionRates[currency01]).toFixed(2);
+//                   game.price_overview = convertedPrice;
+//                 }
+//               }
+//             }
+//               return game;
+//             });
+        
+//             return res.status(200).json(gamesWithModifiedPrice);
+
+//     } catch (error) {
+//     res.status(404).send(error.message);
+//     }
+// };
+// module.exports = {
+//     nameGames
+// }
 
 
 
